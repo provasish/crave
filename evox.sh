623f9cfbdc6868927792d8bf0a3d4699d1070e54
Provasish Nandi <82043200+provasish@users.noreply.github.com>
2026-03-09 12:06:05 +0530

    Refactor evox.sh for cleaner build setup
    Updated build hostname and removed Updater app references.

--- evox.sh
+++ evox.sh
@@ -1,17 +1,10 @@
 #!/bin/bash
 
 rm -rf .repo/local_manifests
-rm -rf packages/apps/Updater
 
 repo init -u https://github.com/Evolution-X/manifest -b bq2 --git-lfs
 
 /opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-#safer sync
-/opt/crave/resync.sh || repo sync -c -j$(nproc --all) --force-sync --no-clone-bundle --no-tags
-
-pushd packages/apps/Updater
-git fetch https://github.com/EvoX-Spartan/packages_apps_Updater && git cherry-pick 07ea7003ea4465d9bdf0fe65b94e860693597989
-popd
 
 rm -rf device/realme
 rm -rf vendor/realme
@@ -45,7 +38,7 @@ git clone https://github.com/olzhas0986/keys vendor/lineage-priv/keys
 
 export TZ=Asia/Kolkata
 export BUILD_USERNAME=hunt3r
-export BUILD_HOSTNAME=pro
+export BUILD_HOSTNAME=crave
 export WITH_GMS=true
 export TARGET_BUILD_VARIANT=user
 
